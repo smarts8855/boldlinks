158e1c95619fd2bf4383a032b10d2dc8bd695109
smarts8855 <olaoyea64@gmail.com>
2025-02-05 13:33:33 +0100

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-
 import Testimonies from "./components/Testimonies";
 import OurCourses from "./components/OurCourses";
 import Service from "./components/Service";
@@ -8,17 +7,12 @@ import Service from "./components/Service";
 function App() {
   return (
     <div>
-      {/* <Header /> */}
       <Hero />
       <Service />
-      {/* <Services /> */}
+
       <OurCourses />
-      {/* <Pricing /> */}
+
       <Testimonies />
-      {/* 
-      {btns.map((btn, index) => (
-        <Button key={index} name={btn} />
-      ))} */}
 
       <Footer />
     </div>
